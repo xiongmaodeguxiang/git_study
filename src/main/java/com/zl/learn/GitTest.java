937eb96470e3991aab4bf100dc6ee999c3827733
zhanglin574 <zhanglin574@xdf.cn>
2020-12-18 09:36:33 +0800

    测试push

---
+++ src/main/java/com/zl/learn/GitTest.java
@@ -0,0 +1,12 @@
+package com.zl.learn;
+
+/**
+ * @Author:zhanglin
+ * @Description:
+ * @Date： 2020/12/18 9:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试提交");
+    }
+}
