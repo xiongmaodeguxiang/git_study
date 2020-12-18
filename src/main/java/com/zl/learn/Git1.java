662eab6eed74ca08d4256a10bc409d493962875c
zhanglin574 <zhanglin574@xdf.cn>
2020-12-18 11:51:37 +0800

    '清空内容'

--- src/main/java/com/zl/learn/Git1.java
+++ src/main/java/com/zl/learn/Git1.java
@@ -7,9 +7,6 @@ package com.zl.learn;
  */
 public class Git1 {
     public static void main(String[] args) {
-        System.out.println("....1");
-        System.out.println("....2");
-        System.out.println("....3");
-        System.out.println("....5");
+
     }
 }
