32bd66dc83d24c89bd2740087815f3a99ff360a2
zhanglin574 <zhanglin574@xdf.cn>
2020-12-18 14:29:16 +0800

    '提交了gitignore文件'

---
+++ src/main/java/com/zl/learn/NewClass.java
@@ -0,0 +1,12 @@
+package com.zl.learn;
+
+/**
+ * @Author:zhanglin
+ * @Description:
+ * @Date： 2020/12/18 14:05
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("新增了文件");
+    }
+}
